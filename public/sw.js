9b840b6340dd9e9ad5c3e2f01041fb47344f0281
bersou <josemoraesbernardo@gmail.com>
2026-01-11 14:54:37 -0300

    fix: enable full PWA standalone mode with robust offline support

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'buscavagas-v2';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        Promise.all([
+            self.clients.claim(),
+            caches.keys().then((cacheNames) => {
+                return Promise.all(
+                    cacheNames.map((cacheName) => {
+                        if (cacheName !== CACHE_NAME) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        ])
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request).then((response) => {
+                    return response || caches.match('/');
+                });
+            })
+    );
+});
